ba3c1f7d8b2035e762d3f49e72c38cdeca23797c
Woozy28 <mazepa.t@icloud.com>
2023-05-11 09:48:51 +1000

    new

--- app/src/main/java/com/example/neweduapp/MainActivity.kt
+++ app/src/main/java/com/example/neweduapp/MainActivity.kt
@@ -8,4 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    class SimpleClass(var h:Int, var w:Int) {
+        constructor(x1:Int,y1:Int,x2:Int,y2:Int):
+                this(y2-y1,x2-x1)
 }
