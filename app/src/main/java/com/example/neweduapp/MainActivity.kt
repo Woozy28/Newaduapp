60ef005e607eede3fbdc5d04d9544054f3ad3bf1
Woozy28 <mazepa.t@icloud.com>
2023-05-11 09:55:11 +1000

    some

--- app/src/main/java/com/example/neweduapp/MainActivity.kt
+++ app/src/main/java/com/example/neweduapp/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.neweduapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    class SimpleClass(var h:Int, var w:Int) {
-        constructor(x1:Int,y1:Int,x2:Int,y2:Int):
-                this(y2-y1,x2-x1)
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
