8b5d141fd3bfd29482e86cb6f3544db39eda274b
Woozy28 <mazepa.t@icloud.com>
2023-05-19 10:11:13 +1000

    some new

--- app/src/main/java/com/example/neweduapp/MainActivity.kt
+++ app/src/main/java/com/example/neweduapp/MainActivity.kt
@@ -4,14 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity() : AppCompatActivity(), Parcelable {
     constructor(parcel: Parcel) : this() {
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener{
+            Log.d("TAG", "button 2 was pressed")
+        }
+
+
+
+        //var et: EditText = findViewById(R.id.editTextText1)
+        //Log.d("TAG", "edittext1.text == ${et.text}")
+        //et.setText("new text")
+
+
+
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
