1e14bd76380a47a6534f4693c5ab2d5c66a24d28
Woozy28 <mazepa.t@icloud.com>
2023-05-23 10:33:43 +1000

    new work

--- app/src/main/java/com/example/neweduapp/MainActivity.kt
+++ app/src/main/java/com/example/neweduapp/MainActivity.kt
@@ -4,25 +4,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 
 class MainActivity() : AppCompatActivity(), Parcelable {
     constructor(parcel: Parcel) : this() {
     }
 
+    fun getnum (n1: Editable, n2: Editable):Array<Int>{
+        val newarray: Array<Int> = arrayOf(n1.toString().toInt(),n2.toString().toInt())
+        return newarray
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button2: Button = findViewById(R.id.button2)
-        button2.setOnClickListener{
-            Log.d("TAG", "button 2 was pressed")
+        //вводы выводы цифр
+        var firstarg: EditText = findViewById(R.id.first_arg)
+        var secondarg: EditText = findViewById(R.id.second_arg)
+        var res: TextView = findViewById(R.id.result)
+
+        //buttons
+        var but_add: Button = findViewById(R.id.add_but)
+        var but_subtr: Button = findViewById(R.id.sub_but)
+        var but_multi: Button = findViewById(R.id.mul_but)
+        var but_dev: Button = findViewById(R.id.dev_but)
+
+        var n1: Int = 0
+        var n2: Int = 0
+        var r1: Int = 0
+
+        but_add.setOnClickListener{
+            n1 = firstarg.text.toString().toInt()
+            n2 = secondarg.text.toString().toInt()
+            r1 = n1 + n2
+            res.setText(r1.toString())
         }
 
+        but_subtr.setOnClickListener{
+            val nums: Array<Int> = getnum(firstarg.text,secondarg.text)
+            res.setText((nums[0]-nums[1]).toString())
+
+        }
+
+        //but_multi.setOnClickListener{
+        //    res = firstarg.text * secondarg.text
+        //}
+
+        //but_dev.setOnClickListener{
+        //    res = firstarg.text / secondarg.text
+        //}
 
 
         //var et: EditText = findViewById(R.id.editTextText1)
