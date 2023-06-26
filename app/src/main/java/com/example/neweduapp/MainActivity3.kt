f8949bcfdc3a42968b27750dc9b3509ee7697e8c
Woozy28 <mazepa.t@icloud.com>
2023-06-27 09:35:56 +1000

    + gridView list

--- app/src/main/java/com/example/neweduapp/MainActivity3.kt
+++ app/src/main/java/com/example/neweduapp/MainActivity3.kt
@@ -3,7 +3,11 @@ package com.example.neweduapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.GridView
+import android.widget.ListView
+
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +17,14 @@ class MainActivity3 : AppCompatActivity() {
         //button
         val gototwo: Button = findViewById(R.id.button_to2)
 
+        var data: Array<Int> = arrayOf(100,200,300)
+
+        var gridView: GridView = findViewById(R.id.gridview)
+        var adapter = ArrayAdapter<Int>(this,android.R.layout.simple_list_item_1, data)
+        gridView.adapter = adapter
+
         gototwo.setOnClickListener {
-            val gotwo =Intent(this,MainActivity2::class.java)
+            val gotwo = Intent(this,MainActivity2::class.java)
             startActivity(gotwo)
         }
     }
